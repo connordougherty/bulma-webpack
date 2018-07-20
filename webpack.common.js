985159f2ac1f7fd170d7dddb8bd6485f4ea50c55
Connor Dougherty <connordougherty01@gmail.com>
2018-07-20 09:36:18 -0400

    Added html-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin (['dist']),
+        new HtmlWebpackPlugin ({
+            title: 'bulma-webpack',
+            template: 'src/index.html'
+        })
+    ]
 };

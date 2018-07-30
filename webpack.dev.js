ac68b31c20c3bd32b81acd3b60043c4850dd09e5
Connor Dougherty <connordougherty01@gmail.com>
2018-07-30 13:40:39 -0400

    Added CSS optimizer

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'development',
@@ -18,4 +19,9 @@ module.exports = merge(common, {
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            template: 'src/index.html'
+        })
+    ]
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,18 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 
 module.exports = merge(common, {
     mode: 'production',
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin({})
+        ]
+    },
     module: {
         rules: [
             {
@@ -17,6 +26,10 @@ module.exports = merge(common, {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin (['dist']),
+        new HtmlWebpackPlugin ({
+            template: 'src/index.html'
+        }),
         new MiniCssExtractPlugin({
             filename: 'styles.css'
         })

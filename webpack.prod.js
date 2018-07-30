e582ec2f892e52edcdd4c67cbae3ae1d56443b4d
Connor Dougherty <connordougherty01@gmail.com>
2018-07-30 14:14:35 -0400

    Made css non-blocking

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
 
 
 module.exports = merge(common, {
@@ -32,6 +34,19 @@ module.exports = merge(common, {
         }),
         new MiniCssExtractPlugin({
             filename: 'styles.css'
+        }),
+        new HtmlCriticalWebpackPlugin({
+            base: path.resolve(__dirname, 'dist'),
+            src: 'index.html',
+            dest: 'index.html',
+            inline: true,
+            minify: true,
+            extract: true,
+            width: 375,
+            height: 565,
+            penthouse: {
+                blockJSRequests: false,
+            }
         })
     ]
 });
